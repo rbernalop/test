808b394a9ee0d876e1e800b5276eeb90c87f6296
Rafael Bernabeu <rafabernabeu3@gmail.com>
2024-08-28 20:59:26 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+sudo apt purge adwaita-icon-theme geogebra-gnome gir1.2-gtd-1.0 \
+gnome-accessibility-profiles gnome-applets-data gnome-audio gnome-backgrounds \
+gnome-cards-data gnome-common gnome-desktop-testing gnome-dvb-daemon \
+gnome-extra-icons gnome-flashback-common \
+gnome-humility-icon-theme gnome-hwp-support gnome-icon-theme \
+gnome-icon-theme-gartoon gnome-icon-theme-gartoon-redux \
+gnome-icon-theme-gperfection2 gnome-icon-theme-nuovo gnome-icon-theme-suede \
+gnome-icon-theme-yasis gnome-mime-data gnome-nds-thumbnailer \
+gnome-packagekit-data gnome-panel-control gnome-panel-data \
+gnome-pkg-tools gnome-recipes-data gnome-remote-desktop gnome-settings-daemon-dev \
+gnome-shell-pomodoro-data gnome-software-common gnome-software-doc \
+gnome-theme-gilouche gnome-video-effects-extra gnome-video-effects-frei0r \
+guile-gnome2-dev guile-gnome2-glib libgnome-autoar-doc libgnomecanvas2-common \
+libgnomecanvas2-doc libgnomecanvasmm-2.6-doc libgnome-panel-doc libgnome-todo-dev \
+libopenrawgnome7:amd64 libopenrawgnome-dev libreoffice-gnome libxine2-gnome:amd64 \
+nautilus-sendto pidgin-gnome-keyring plymouth-theme-ubuntu-gnome-logo \
+plymouth-theme-ubuntu-gnome-text ubuntu-gnome-wallpapers \
+ubuntu-gnome-wallpapers-trusty ubuntu-gnome-wallpapers-utopic \
+ubuntu-gnome-wallpapers-xenial ubuntu-gnome-wallpapers-yakkety
+sudo apt purge gnome-exe-thumbnailer
+sudo apt purge slim openbox geoclue-2.0
+
+sudo apt autopurge
